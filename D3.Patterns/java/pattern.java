b879a6e4520540da5eda6a31686e05a8678c12f6
mayankraghuwanshi <mayank1272@hotmail.com>
2019-09-21 00:50:12 +0530

    one more pattern added.

--- D3.Patterns/java/pattern.java
+++ D3.Patterns/java/pattern.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class main{
@@ -228,9 +229,52 @@ class main{
 //     1 3 3 1
 //    1 4 6 4 1 
 
-
+    public static void pattern8(int n){
+        int b=(n%2==0)?n/2:n/2+1;
+        int i=0;
+        int rowEnd=n;
+        int colEnd=n;
+        int start=0;
+        int arr[][] = new int[n][n];
+        int count=1;
+        while(i<b){
+            for(int r=start;r<rowEnd;r++){
+                //fill all row's first col with count
+                arr[r][start]=count;
+                //fill all row's last col woth count
+                arr[n-r-1][n-start-1]=count;
+            }
+            for(int c=start;c<colEnd;c++){
+                //fill all col in first row with count
+                arr[start][c]=count;
+                //fill all col in last row with count
+                arr[n-start-1][n-c-1]=count;
+            }
+            count++;
+            start++;
+            rowEnd--;
+            colEnd--;
+            i++;
+        }
+        for(int r=0;r<n;r++){
+            for(int c=0;c<n;c++){
+                System.out.print(arr[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
+//OUTPUT-
+//1 1 1 1 1 1 1 1 1
+//1 2 2 2 2 2 2 2 1
+//1 2 3 3 3 3 3 2 1
+//1 2 3 4 4 4 3 2 1
+//1 2 3 4 5 4 3 2 1
+//1 2 3 4 4 4 3 2 1
+//1 2 3 3 3 3 3 2 1
+//1 2 2 2 2 2 2 2 1
+//1 1 1 1 1 1 1 1 1
     public static void main(String args[]){
         int n=9;
-        pattern3(n);
+        pattern8(n);
     }
 }
